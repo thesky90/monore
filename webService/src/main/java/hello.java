2c904124673cdacfa2aed7fb83af83fc14b999ee
hansel chong <hansel.chong@gmail.com>
2015-09-19 18:55:43 -0700

    initial check in

---
+++ webService/src/main/java/hello.java
@@ -0,0 +1,11 @@
+package main.java;
+
+/**
+ * Created by Hansel Chong on 9/19/2015.
+ */
+public class hello {
+
+    public static void main(String[] args){
+        System.out.println("test");
+    }
+}
