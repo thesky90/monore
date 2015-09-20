f6c63eec86ae4c3788457f482676bb12d6718e1e
hansel chong <hansel.chong@gmail.com>
2015-09-20 14:01:08 -0700

    inital push - working local

---
+++ webService/src/main/java/com/monore/Application.java
@@ -0,0 +1,15 @@
+package com.monore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Hansel Chong on 9/20/2015.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
